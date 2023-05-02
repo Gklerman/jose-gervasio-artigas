f57c25f936aadaea4b17119d6da43576ddbb185d
Germán <germanaraujo@hotmail.com>
2023-05-02 02:15:23 -0300

    Working on php version

---
+++ php/validate_form.php
@@ -0,0 +1,17 @@
+<?php
+
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = test_input($_POST["name"]);
+        $email = test_input($_POST["email"]);
+        $message = test_input($_POST["message"]);
+    }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+?>
